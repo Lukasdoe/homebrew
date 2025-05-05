efdf8694f699d52bc5f503d239dd0ad4e705008b
Lukas Döllerer <lukas.doellerer@tum.de>
2025-05-05 12:57:38 +0200

    remove apache-arrow bottle

--- Formula/a/apache-arrow@20.rb
+++ Formula/a/apache-arrow@20.rb
@@ -1,4 +1,4 @@
-class ApacheArrow < Formula
+class ApacheArrowAT20 < Formula
     desc "Columnar in-memory analytics layer designed to accelerate big data"
     homepage "https://arrow.apache.org/"
     url "https://www.apache.org/dyn/closer.lua?path=arrow/arrow-20.0.0/apache-arrow-20.0.0.tar.gz"
@@ -6,16 +6,6 @@ class ApacheArrow < Formula
     sha256 "89efbbf852f5a1f79e9c99ab4c217e2eb7f991837c005cba2d4a2fbd35fad212"
     license "Apache-2.0"
     head "https://github.com/apache/arrow.git", branch: "main"
-
-    bottle do
-      sha256 cellar: :any,                 arm64_sequoia: "09dd6b12173701dbbefa75a850d7d1ee2eef7611e46f3defa2ba1f60179bb0b2"
-      sha256 cellar: :any,                 arm64_sonoma:  "32d41bb6a695b9d5bc7ab70c59519fdef495021bf66a60c53a209affe84e8fb0"
-      sha256 cellar: :any,                 arm64_ventura: "ef576b75ee40955a2a31e951d779c1a7dffa1f46d494180a2f76f3328f223bdf"
-      sha256 cellar: :any,                 sonoma:        "0b0ff31763614549f15221730f2930ec21b29130ac8b4a2ff3a88f74be857856"
-      sha256 cellar: :any,                 ventura:       "1b33d5a46e8d4152fa089548888c26891ac07784ae295c442188fe196bf3158e"
-      sha256 cellar: :any_skip_relocation, arm64_linux:   "b5585805a7d0146b0c1ba82f6c57a8f374c61e15ebc4182cb934b1a6c5f13385"
-      sha256 cellar: :any_skip_relocation, x86_64_linux:  "3527eef2d7240e0b7087621d85bb3bb010a25e8ee6a0c498e99b36c505c6772f"
-    end
 
     depends_on "boost" => :build
     depends_on "cmake" => :build
